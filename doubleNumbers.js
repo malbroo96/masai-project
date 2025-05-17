1ac4a055b7867dd8b06cf472d0ea167e22aa3dbc
malbroo96 <akhiljoseph225292@gmail.com>
2025-05-17 11:35:01 +0530

    i need to pratice more mapping

---
+++ doubleNumbers.js
@@ -0,0 +1,10 @@
+function doubleNumbers(numbers){
+    return numbers.map(num=>num*2)
+
+}
+
+
+const numbers =[1,2,3,4,5,6,7,8,9,10]
+const result =doubleNumbers(numbers)
+console.log(result)
+console.log(numbers)
