30e1e408c255b82f95ce4e507aa0deba293de4ea
malbroo96 <akhiljoseph225292@gmail.com>
2025-07-02 22:28:15 +0530

    with comments

--- fetch1.js
+++ fetch1.js
@@ -1,11 +1,11 @@
-async function Fetchucserdata(){
-    try{
-        let res=await fetch('https://fakestoreapi.com/products')
-        let products=await res.json()
-        products.forEach((product)=>{console.log(product.title)})
-    let total=products.reduce((acc,product)=>{return acc + product.price},0)
-    console.log("total price :",total.toFixed(2))
-    }catch(error){
-        console.log("error while loading please try again !")
+async function Fetchucserdata(){//start with async keyword
+    try{//then this is resolve 
+        let res=await fetch('https://fakestoreapi.com/products')//use await for waiting for the result 
+        let products=await res.json()//to extract the json data to readable 
+        products.forEach((product)=>{console.log(product.title)})//ittrate through each element to view title
+    let total=products.reduce((acc,product)=>{return acc + product.price},0)//ittrate each price to find total
+    console.log("total price :",total.toFixed(2))// to fixed is to redefine the point value to max 2
+    }catch(error){//to deal with errors
+        console.log("error while loading please try again !")//error message
     }
-}Fetchucserdata()
+}Fetchucserdata()//to invoke
