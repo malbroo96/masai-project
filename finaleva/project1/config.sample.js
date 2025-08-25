c9ceee48a401071ef5fc40b57e049dd069733b40
malbroo96 <akhiljoseph225292@gmail.com>
2025-08-25 21:52:18 +0530

    moved creds to common config file

---
+++ finaleva/project1/config.sample.js
@@ -0,0 +1,14 @@
+// config.js
+const HF_API_TOKEN = "your_huggingface_api_token_here";
+const HF_API_URL =
+  "https://api-inference.huggingface.co/models/facebook/bart-large-mnli";
+const NEWS_API_KEY = "your_newsapi_key_here";
+const firebaseConfig = {
+  apiKey: "ABCD",
+  authDomain: "wealthy-cac7b.firebaseapp.com",
+  projectId: "wealthy-cac7b",
+  storageBucket: "wealthy-cac7b.appspot.com",
+  messagingSenderId: "819937615061",
+  appId: "1:819937615061:web:0b36637c108535a56e565c",
+  measurementId: "G-9GF1PS2LHZ",
+};
--- finaleva/project1/configuration.js
+++ finaleva/project1/configuration.js
@@ -1,19 +1,14 @@
 // Firebase SDKs
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-app.js";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/11.0.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
-// Your Firebase config
-const firebaseConfig = {
-  apiKey: "AIzaSyCjJGfH2i_WSlQllKTpIF8zB4yLoBVyCwo",
-  authDomain: "wealthy-cac7b.firebaseapp.com",
-  projectId: "wealthy-cac7b",
-  storageBucket: "wealthy-cac7b.appspot.com",
-  messagingSenderId: "819937615061",
-  appId: "1:819937615061:web:0b36637c108535a56e565c",
-  measurementId: "G-9GF1PS2LHZ",
-  databaseURL: "https://wealthy-cac7b-default-rtdb.firebaseio.com/" // 👈 add this
-};
+import config from "../../config.js";
+const firebaseConfig = config.firebaseConfig;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
@@ -25,14 +20,14 @@ const db = getFirestore(app); // Use Firestore instead of Realtime Database
 // =================
 document.getElementById("registerForm").addEventListener("submit", (e) => {
   e.preventDefault();
-  
+
   const name = document.getElementById("registerName").value;
   const email = document.getElementById("registerEmail").value;
   const password = document.getElementById("registerPassword").value;
 
   createUserWithEmailAndPassword(auth, email, password)
     .then((userCredential) => {
-        console.log("User registered:", userCredential.user);
+      console.log("User registered:", userCredential.user);
       const user = userCredential.user;
       console.log("User registered:", user);
 
