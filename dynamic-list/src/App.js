54eace0df342a86a94a40565816c8918b8a36fe7
malbroo96 <akhiljoseph225292@gmail.com>
2025-08-29 07:20:21 +0530

    done somthing

---
+++ dynamic-list/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  const items = ["React", "JavaScript", "CSS"];
+
+  return (
+    <div>
+      <h1>My List</h1>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
